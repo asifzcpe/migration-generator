5cfca2d0c9829adbb5f31e4933867d39c44f535d
asifzcpe <rahmanasif05004@gmail.com>
2020-03-01 19:31:41 +0600

    removed

--- migration-generator/src/Factories/MigrationGeneratorInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Asif\MigrationGenerator\Factories;
-interface MigrationGeneratorInterface
-{
-    public function makeCommand(string $columnName,bool $isNullable=false, string $modificationType);
-}
